d38eb6411631b9a17128f16c9d68d8982c4b8146
Abin Joeph <itsmeabjs@gmail.com>
2020-12-27 18:12:25 +0530

    Jset an commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './App.css';
+import HomeInUp from './Page/HomeInUp'
 export default function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <HomeInUp/>
     </div>
   )
 }
---
+++ src/Page/HomeInUp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Header from '../Component/Header/Header'
+export default function HomeInUp() {
+    return (
+        <div>
+            <Header/>
+        </div>
+    )
+}
